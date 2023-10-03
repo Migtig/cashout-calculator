4b3e4c094ab3f76d9df8b140bce372d7a57f1e9c
Connor <cfroese4@my.bcit.ca>
2023-10-02 18:36:04 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cashout Calculator</title>
+    <link rel="stylesheet" href="styles/normalize-fwd.css">
+
+</head>
+<body>
+
+<main>
+    <form action="" method="post">
+        <fieldset>
+            <legend>Cashout</legend>
+
+            <label for="sales">Net Sales</label>
+            <input type="text" name="sales" id="sales">
+
+            <label for="food">Food</label>
+            <input type="text" name="food" id="food">
+
+            <label for="cash">Cash</label>
+            <input type="text" name="cash" id="cash">
+
+            <label for="tips">Tips Paid</label>
+            <input type="text" name="tips" id="tips">
+        </fieldset>
+
+        <input type="submit" value="Submit">
+    </form>
+</main>
+    
+</body>
+</html>
