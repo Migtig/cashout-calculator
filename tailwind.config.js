88f283ab6f64fbfa0213d02020c254091271f150
Connor <cfroese4@my.bcit.ca>
2023-10-04 14:05:01 -0700

    configured tailwind and added basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,php}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
